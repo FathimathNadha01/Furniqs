20eaea2110effb12d3a67f5810865edb94681333
FathimathNadha01 <nadhaanees777@gmail.com>
2025-04-18 13:14:57 +0530

    upadted ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/layout/Footer'
+import Product from './pages/Product'
+import Carousel from './components/Carousel'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +12,8 @@ function App() {
   return (
     <>
       <h1 className='text-red-700'>hello</h1>
+      <Product/>
+      <Carousel/>
     </>
   )
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <div>
+        <h1>Funiro.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, corrupti?</p>
+      </div>
+      <div>
+        <p>Links</p>
+        <p>Home</p>
+        <p>Shop</p>
+        <p>Contact</p>
+        <p>About</p>
+      </div>
+      <div>
+        <p>Help</p>
+        <p>payment options</p>
+        <p>Returns</p>
+        <p>Privcy Policies</p>
+      </div>
+      <div>
+        <p>Newsletter</p>
+        <input type="text" placeholder='enter your email address' />
+        <p>Subscribe</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
